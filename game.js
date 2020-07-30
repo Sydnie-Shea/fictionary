380ee532b3b899d29ba9517242484b99cae47b6e
Sydnie-Shea <sydnie_shea@berkeley.edu>
2020-07-30 17:04:11 -0400

    added game class

--- game.js
+++ game.js
@@ -1,7 +1,8 @@
 class Game {
-    constructor() {
+    constructor(diction) {
         this.players = []; 
-        this.currentHolder = 0;       
+        this.currentHolder = 0;  
+        this.dictionary = diciton; 
     }
     addPlayer(player) {
         this.players.push(player);
@@ -10,11 +11,53 @@ class Game {
     removePlayer(player){
         this.players.pop(player);
     }
-
-    round() {
+    startRound() {
+        this.currentFakeDef = {};
         this.currentHolder += 1;
+        this.correctGuess = 0;
         if (this.currentHolder == length(this.players)) {
-            
+            this.currentHolder = 0;
+        }
+        this.currentHolderPlayer = this.players[this.currentHolder];
+        return this.currentHolderPlayer;
+    }
+
+    determineWord() {
+        this.currentWordInfo = this.dictionary.getNewWord()
+        this.currentWord = this.currentWordInfo["word"];
+        this.currentDefinition = this.currentWordInfo["definition"]
+        return this.currentWordInfo;
+    }
+
+    fakeDefinition(player, definitionGuess) {
+        this.currentFakeDef[player] = definitionGuess;
+    }
+
+    goodDefinition(player) {
+        player.scoreChange(2);
+        delete this.currentFakeDef[player];
+    }
+
+    showDefinitions() {
+        this.currentFakeDef["actual"] = this.currentDefinition;
+        return this.currentFakeDef;
+    }
+
+    guess(player, guess) {
+        if (guess == "actual") {
+            this.correctGuess += 1;
+            player.scoreChange(1);
+        } else {
+            guess.scoreChange(1);
         }
     }
+
+    endRound() {
+        if (this.correctGuess > 0) {
+            this.currentHolderPlayer.scoreChange(2);
+        }
+    }
+
+
+
 }
--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.get('/', function(req, res){
 });
 
 io.on('connection', function(socket){
-  //alert("name?")
+  name = prompt("What is your name?")
   //create player
   //game.add(player)
   socket.on('chat message', function(msg){
