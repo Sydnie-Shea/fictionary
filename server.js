b8a2ab2b54817e3ef9a5628ae65a6ddd616b39bc
Sarah Bhaskaran <sbhaskaran1357@berkeley.edu>
2020-08-11 21:12:49 -0700

    added express

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = process.env ? require('socket.io')(http) : require('socket.io')(3000);
+
 const Game = require('./game.js');
 const Player = require('./player.js');
 const Dictionary = require('./dictionary.js');
-
-const io = require('socket.io')(process.env.PORT || 3000)
 
 const users = {}
 
